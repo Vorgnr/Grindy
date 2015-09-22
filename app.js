2f6471c035c69944db9462dc17f69524fbb09e5c
Vorgnr <arnaud.cavat@gmail.com>
2015-09-23 01:21:40 +0200

    clean app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var app = express();
 var server = require('http').Server(app);
 
 app.use(express.static('dist'));
-app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
