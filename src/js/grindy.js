16fbdf93fcaa5dc778e76a4b1165023ab6ccc296
Adrien Louis-Rossignol <adrien.louis.r@gmail.com>
2015-09-24 20:03:59 +0200

    Showing some results in browser \o/

--- src/js/grindy.js
+++ src/js/grindy.js
@@ -2,38 +2,51 @@ import Hero from './hero.js'
 import Monster from './monster.js'
 import Rx from 'rx'
 
+const round = (fight, monster) => {
+  if (monster.current_life() > 0) {
+    fight.onNext(monster)
+  } else {
+    fight.onCompleted()
+  }
+}
+
 export default () => {
   const player = Hero()
+  const gameLayerElement = document.querySelector('.game-layer')
+  const spawnButton = document.querySelector('#spawn-monster')
+  const monsters = Rx.Observable.fromEvent(spawnButton, 'click')
 
   const fight = Rx.Observable.create((fight) => {
     const monster = Monster()
-    const one = setInterval(() => {
-      if (monster.current_life() > 0) {
-        fight.onNext(monster)
-      } else {
-        fight.onCompleted()
-      }
-    }, player.as() * 1000)
-    console.log('Fight!')
+    const rounds = setInterval(
+      () => round(fight, monster),
+      player.as() * 1000
+    )
+    gameLayerElement.innerHTML = ''
+    gameLayerElement.innerHTML += '<p><strong>Fight!</strong></p>'
 
-    return () => clearInterval(one)
-  })
-
-  const fightSubscribe = fight.subscribe(
-    (monster) => {
-      console.log('Player attack for: %d', player.damage())
-      monster.receive_attack(player.damage())
-      console.log('Monster life left : %d', monster.current_life())
-    },
-    err => console.log('Error: %s', err),
-    () => {
-      console.log('You defeated, Hurrah for loots!')
+    return () => {
+      clearInterval(rounds)
     }
+  }).doOnCompleted(
+    () => gameLayerElement.innerHTML += '<p><strong>You defeated, Hurrah for loots!</strong></p>'
+  )
+
+  const game = monsters
+    .startWith('startup click')
+    .flatMap(monster => fight)
+
+  const gameSubscribe = game.subscribe(
+    (monster) => {
+      gameLayerElement.innerHTML += `<p>Player attack for: ${player.damage()}</p>`
+      monster.receive_attack(player.damage())
+      gameLayerElement.innerHTML += `<p>Monster life left: ${monster.current_life()}</p>`
+    },
+    err => console.error('Error: %s', err),
+    () => console.log('End of the game Bobby')
   )
 
   return {
-    start: () => {
-      fightSubscribe
-    }
+    start: () => gameSubscribe
   }
 }
