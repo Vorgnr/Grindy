2129e1407d5539e8b123c2ef3ef47bb9fde8d622
Vorgnr <arnaud.cavat@gmail.com>
2015-09-25 15:44:38 +0200

    hero rewards system

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,9 +1,27 @@
+import Logger from './logger.js'
+import Rx from 'rx'
+
 export default () => {
+  const logger = Logger()
   const damage = 1
-  const attack_speed = 0.5
+  const attackSpeed = 0.5
+  let exp = 0
+  let chest = {
+    gold: 0,
+    items: []
+  }
+
+  const grantRewards = (reward) => {
+    exp += reward.exp
+    chest.gold += reward.chest.gold
+    chest.items.concat(reward.chest.items)
+
+    logger.log(`New gold value ${chest.gold}`)
+  }
 
   return {
     damage: () => damage,
-    as: () => attack_speed
+    as: () => attackSpeed,
+    grantRewards: grantRewards
   }
 }
