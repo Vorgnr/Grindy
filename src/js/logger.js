bb233bc18be69911ecd8fbd64b3465da21e036de
Vorgnr <arnaud.cavat@gmail.com>
2015-09-25 15:43:33 +0200

    Add chest to monster

--- src/js/logger.js
+++ src/js/logger.js
@@ -1,4 +1,3 @@
-
 export default () => {
   return {
     log: (str) => {
--- src/js/monster.js
+++ src/js/monster.js
@@ -1,8 +1,17 @@
 export default () => {
   let life = 5
+  let exp: 1
+  const chest = {
+    gold: 5,
+    items: []
+  }
 
   return {
     current_life: () => life,
-    receive_attack: (damage) => life -= damage
+    receive_attack: (damage) => life -= damage,
+    isDead: () => life <= 0,
+    reward: () => {
+      return { exp, chest }
+    }
   }
 }
