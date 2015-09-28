913299ff4cfa6c21a1dbd779399e51c8a9f5c1a9
Adrien Louis-Rossignol <adrien.louis.r@gmail.com>
2015-09-28 18:54:19 +0200

    Remove useless parameter

--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,16 @@ import '../css/main.scss'
 import 'file?name=[name].html!../index.html'
 import './utils/extensions.js'
 import Grindy from './core/grindy.js'
-import {GrindyDOM, subscribe} from './components/app.js'
+import {subscribe} from './components/app.js'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Rx from 'rx'
 
 const game = Grindy()
 const { appState, initState } = game.initState()
-const App = subscribe(GrindyDOM, { player: appState }, { player: initState })
+const App = subscribe({ player: appState }, { player: initState })
 ReactDOM.render(<App />, document.getElementById('grindy'))
+
 const spawnButton = document.querySelector('#spawn-monster')
 const clicks = Rx.Observable.fromEvent(spawnButton, 'click')
 game.start(clicks)
