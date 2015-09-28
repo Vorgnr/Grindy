4373b8bfedc8b871a6b7bfbaef0c2ed2d03e388c
Adrien Louis-Rossignol <adrien.louis.r@gmail.com>
2015-09-28 21:12:53 +0200

    * Whistles *

--- src/js/utils/storage.js
+++ src/js/utils/storage.js
@@ -5,7 +5,6 @@ const Storage = () => {
     save: (data) => window.localStorage.setItem(fileName, window.btoa(JSON.stringify(data))),
     load: () => {
       const item = window.localStorage.getItem(fileName)
-      console.debug(item)
       if (item) {
         return JSON.parse(window.atob(item))
       } else {
