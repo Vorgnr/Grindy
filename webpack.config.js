c4c1575c9427a9131ea604da9369469bda0ff613
Vorgnr <arnaud.cavat@gmail.com>
2015-10-02 00:18:04 +0200

    Add file-loader and svg compressor

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,14 @@
 var path = require('path')
 var node_modules = path.resolve(__dirname, 'node_modules')
 
+var svgoConfig = JSON.stringify({
+  plugins: [
+    {removeTitle: true},
+    {convertColors: {shorthex: false}},
+    {convertPathData: false}
+  ]
+})
+
 module.exports = {
   entry: path.resolve(__dirname, 'src/js/index.js'),
   output: {
@@ -12,7 +20,8 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, exclude: node_modules, loader: 'babel-loader?stage=0' },
-      { test: /\.scss$/, exclude: node_modules, loader: 'style!css!sass' }
+      { test: /\.scss$/, exclude: node_modules, loader: 'style!css!sass' },
+      { test: /\.svg$/, exclude: node_modules, loader: 'file-loader!svgo-loader' }
     ]
   },
   devServer: { contentBase: './dist' }
